32edebc7e35e0778aca0822b9e95c89c804e9cd4
JannatulHappy <jannatulferdoushappy21@gmail.com>
2022-04-27 12:44:59 +0600

    loginRedux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-     Hello 
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
